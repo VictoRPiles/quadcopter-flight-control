cfcbd7c194e82ff7f2b7aad8d7f93776ee8029ba
Victor <victorpilesdelgado@gmail.com>
2025-05-13 20:36:17 +0200

    Trying to figure out the best way of implementing the altitude and roll
    Added comments and refactored some code

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
-#include <math.h>
-int altitude;
+float altitude;
 int altitude_min;
 int altitude_max;
 
-int altitude_potentiometer;
-int roll_potentiometer;
-float motor_speed, left_motor_speed, right_motor_speed;
+float roll;
+float roll_min;
+float roll_max;
+
+float pot_altitude;
+float pot_roll;
 
 /**
-Read the value from a potentiometer connected to the specified channel
-*/
+ * Read the analog value from a potentiometer connected to the specified channel
+ *
+ * @param channel Channel to read from
+ * @return The analog value of the potentiometer
+ */
 float read_potentiometer(const int channel) {
     ADMUX = (1 << REFS0) | (channel & 0x0F); /* AVCC as reference, select ADC channel */
     ADCSRA |= (1 << ADSC); /* Start conversion */
@@ -20,30 +25,48 @@ float read_potentiometer(const int channel) {
 }
 
 /**
-Scale an ADC value to a percentage
-*/
+ * Scale an ADC value to a percentage
+ *
+ * @param value Analog value
+ * @return Analog value scaled to percentage
+ */
 float scale_adc_to_percent(const float value) {
     return ((float) value / 1023.0f) * 100.0f;
 }
 
-void control_altitude(const int altitude_potentiometer) {
-    /* Modify altitude */
-    // Control altitude with potentiometer
-    if (altitude_potentiometer < 50) {
-        altitude--;
-    } else if (altitude_potentiometer > 50) {
-        altitude++;
-    }
-
-    if (altitude < 0) {
-        /* Quadcopter reached ground level */
-        altitude = 0;
-    } else if (altitude > altitude_max) {
-        /* Quadcopter reached ceiling altitde */
-        altitude = altitude_max;
-    }
+/**
+ * Constrain the resolution of a value within a range
+ *
+ * @param x Value
+ * @param min_val Lower limit
+ * @param max_val Upper limit
+ * @return A value inside the limits
+ */
+float clamp(const float x, const float min_val, const float max_val) {
+    return (x < min_val) ? min_val : (x > max_val) ? max_val : x;
 }
 
+// TODO: FIX THIS TWO
+void control_altitude(const int nominal_speed_percentage) {
+    /* Clamp input to [0, 100] */
+    const int percent = (int) clamp(nominal_speed_percentage, 0, 100);
+    /* Scale by 100 -> Avoid using floating point */
+    const int16_t climb = ((percent - 50) * 2);
+    /* Rescale */
+    altitude += (float) climb * 0.01f;
+    /* Clamp altitude */
+    altitude = clamp(altitude, altitude_min, altitude_max);
+}
+
+void control_roll(const int nominal_speed_percentage) {
+    /* Clamp input to [0, 100] */
+    const float percent = clamp(nominal_speed_percentage, 0, 100);
+
+    /* Linearly map percentage to roll */
+    roll = (roll_min + ((roll_max - roll_min) * percent) / 100.0f);
+}
+
+//TODO: Parameters (refresh rate, digits, altitude...)
 void display_altitude() {
     /* Get the digits of the altitude value */
     unsigned int n_altitude_digits = 3;
@@ -69,7 +92,8 @@ void display_altitude() {
 
     /* Delay */
     // Control delay depending on motor speed (potentiometer value)
-    for (volatile long delay = 0; delay < 10000; delay++) {}
+    for (volatile long delay = 0; delay < 10000; delay++) {
+    }
 
     /* Free allocated memory */
     free(altitude_digits);
@@ -79,8 +103,12 @@ void display_altitude() {
 }
 
 /**
-Split a number in digits and return as an array
-*/
+ * Split a number in digits and return as an array
+ *
+ * @param number Integer number
+ * @param n_digits Number of digits to extract
+ * @return Array with the digits
+ */
 int *digits(int number, const int n_digits) {
     int *digits = (int *) malloc(n_digits * sizeof(int));
     /* Safety check */
@@ -95,8 +123,13 @@ int *digits(int number, const int n_digits) {
 
 
 /**
-Send an array of bits and clock pulse over PIND
-*/
+ * Send an array of bits and clock pulse over PIND
+ *
+ * @param bits Data to be sent
+ * @param n_bits Number of bits sent
+ * @param channel_d Data channel of the display
+ * @param channel_clk Clock channel of the display
+ */
 void sendBits(const int *bits, const int n_bits, const unsigned int channel_d, const unsigned int channel_clk) {
     /* Clear ports before setting new bits */
     PORTD &= ~((1 << channel_d) | (1 << channel_clk));
@@ -115,8 +148,12 @@ void sendBits(const int *bits, const int n_bits, const unsigned int channel_d, c
 }
 
 /**
-Extract the bits of a decimal number to an array
-*/
+ * Extract the bits of a decimal number to an array
+ *
+ * @param decimal A decimal number
+ * @param n_bits Number of bits to extract
+ * @return An array with the bits
+ */
 int *decToBin(const int decimal, const int n_bits) {
     int *bits = (int *) malloc(n_bits * sizeof(int));
     for (int i = n_bits - 1; i >= 0; i--) {
@@ -125,6 +162,9 @@ int *decToBin(const int decimal, const int n_bits) {
     return bits;
 }
 
+/**
+ * Configures the board and initialises the setup variables
+ */
 void setup() {
     /* Configure I/O */
     DDRB = 0b00000000; /* Set [PB0-PB7] as input */
@@ -137,31 +177,26 @@ void setup() {
     altitude_min = 0; /* Ground altitude of the quadcopter */
     altitude_max = 999; /* Ceiling altitude of the quadcopter */
     altitude = altitude_min; /* Altitude starts at ground level */
+
+    roll_min = -45;
+    roll_max = 45;
+    roll = roll_min;
 }
 
+/**
+ * Main execution loop
+ */
 void loop() {
     /* If the controller is ON (PB0 high level) */
     if ((PINB & (1 << 0))) {
-        altitude_potentiometer = scale_adc_to_percent(read_potentiometer(0));
-        roll_potentiometer = scale_adc_to_percent(read_potentiometer(1));
-        /* Total motor speed: Ms = 0.9Ap + 0.1Rp */
-        float altitude_speed = 0.9f * (altitude_potentiometer - 50) * 2;
-        float roll_speed = 0.1f * (roll_potentiometer - 50) * 2;
+        pot_altitude = scale_adc_to_percent(read_potentiometer(0));
+        pot_roll = scale_adc_to_percent(read_potentiometer(1));
 
-        /* Generate torque with the speed difference */
-        if (roll_speed > 0) {
-            left_motor_speed = altitude_speed + roll_speed;
-            right_motor_speed = altitude_speed - roll_speed;
-        } else if (roll_speed < 0) {
-            left_motor_speed = altitude_speed - roll_speed;
-            right_motor_speed = altitude_speed + roll_speed;
-        } else {
-            left_motor_speed = right_motor_speed = altitude_speed;
-        }
-        /* Motor speed in absolute value */
-        motor_speed = altitude_speed + abs(roll_speed);
+        const float speed_altitude = 0.75f * pot_altitude;
+        const float speed_roll = 0.25f * pot_roll;
 
-        control_altitude(motor_speed);
+        control_altitude(pot_altitude);
+        control_roll(pot_roll);
         display_altitude();
     }
 }
